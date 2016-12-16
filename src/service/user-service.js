9f399476327d19254f90d1e259974ca19c100012
RickFang666 <rickfang0123@gmail.com>
2016-12-16 19:36:24 +0800

    login

---
+++ src/service/user-service.js
@@ -0,0 +1,27 @@
+var _mm = require('util/mm.js')
+
+var _user = {
+    login: function (userInfo, resolve, reject) {
+        _mm.request({
+            url: _mm.getServerUrl('/user/login.do'),
+            data: userInfo,
+            method: 'POST',
+            success: resolve,
+            error: reject
+        })
+    },
+    checkUsername: function (username, resolve, reject) {
+        _mm.request({
+            url: _mm.getServerUrl('/user/check_valid.do'),
+            data: {
+                type: 'username',
+                str: username
+            },
+            method: 'POST',
+            success: resolve,
+            error: reject
+        })
+    }
+}
+
+module.exports = _user
